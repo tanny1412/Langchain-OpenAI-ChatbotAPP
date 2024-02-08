552d5992b381a5d479d4bad2d5926a0a7ec59980
Tanish Kandivlikar <tanish.kandivlikar1412@gmail.com>
2024-02-08 14:56:17 -0500

    handling comments

--- app.py
+++ app.py
@@ -4,17 +4,17 @@ from langchain.schema import HumanMessage, SystemMessage, AIMessage
 from dotenv import load_dotenv
 import os
 
-# Load environment variables and print the loaded API key for debugging
+# Load environment variables
 load_dotenv()
 
 # Initialize the ChatOpenAI model
-chat_model = ChatOpenAI(temperature=0.5)  # Assuming the API key is picked from the environment
+chat_model = ChatOpenAI(temperature=0.5)  # the API key is picked from the environment
 
 # Streamlit UI setup
 st.set_page_config(page_title="Conversational Q&A Chatbot")
 st.header("Hey, Let's Chat")
 
-# Initialize conversation messages if not already present
+# Initialize conversation messages if not present
 if 'flowmessages' not in st.session_state:
     st.session_state['flowmessages'] = [SystemMessage(content="You are an AI sports assistant.")]
 
@@ -24,19 +24,19 @@ def get_chatmodel_response(question):
     st.session_state['flowmessages'].append(HumanMessage(content=question))
     
     # Generate the answer from the chat model
-    response = chat_model.invoke(st.session_state['flowmessages'])  # Adjusted to use invoke method
+    response = chat_model.invoke(st.session_state['flowmessages'])  
     
     # Append the AI message
     st.session_state['flowmessages'].append(AIMessage(content=response.content))
     
     return response.content
 
-# User input handling
+# User input
 input_question = st.text_input("Input:", key="input")
 submit = st.button("Ask the question")
 
 # Display the response when the button is clicked
-if submit and input_question:  # Check to ensure input is provided before processing
+if submit and input_question:  # input is provided before processing
     response = get_chatmodel_response(input_question)
     st.subheader("The Response is")
     st.write(response)
